caafabd54e7c05878b511d21cc61e775a509a483
haider <haiderlek@gmail.com>
2021-08-15 11:27:16 +0300

    commit tree 8ba66068ae2179607964da453ee0d1dce216612c

--- src/components/CounterState.js
+++ src/components/CounterState.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 function CounterState() {
-  const [count, setCount] = useState(0);
+  const count = useRef(0);
+  console.log(count);
 
-  function handleClick() {
-    setCount((count) => count + 1);
-  }
-
+  function handleClick() {}
+  count.current = count.current + 1;
+  console.log(count.current);
   return (
     <div>
       <h1>CounterState</h1>
-      <button onClick={handleClick}>{count}</button>
+      <button onClick={handleClick}>{count.current}</button>
     </div>
   );
 }
